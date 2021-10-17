2d85e3de5c28951c9dfda1cf7a9702fa11912a22
alex-kay <ichijoint@gmail.com>
2021-10-18 02:43:44 +0300

    created folders, set ownership

--- README.md
+++ README.md
@@ -7,8 +7,8 @@
 * ~~setup smtp server~~
 * setup slack
 * setup github
-* Create three folders `/folder1`, `/folder1/folder2` and `folder3`
-* for `folder1` configure your shared library
+* ~~Create three folders `/folder1`, `/folder1/folder2` and `folder3`~~
+* ~~for `folder1` configure your shared library~~
 * create credentials `USERNAME` and `PASSWORD`
 * create group and role `poweruser` and assing it to `folder1`
 * inside folder3 create test-job with build permissions for `poweruser`
--- hookScript.groovy
+++ hookScript.groovy
@@ -7,15 +7,24 @@ import jenkins.model.*;
 import hudson.tools.*;
 import hudson.tasks.Mailer
 import hudson.util.Secret;
+import jenkins.plugins.git.GitSCMSource
 import jenkins.plugins.slack.SlackNotifier.*
 import org.jenkinsci.plugins.github.GitHubPlugin
 import org.jenkinsci.plugins.github.config.GitHubServerConfig
 import com.cloudbees.hudson.plugins.folder.Folder
 import org.jenkinsci.plugins.ownership.model.folders.FolderOwnershipHelper
+import org.jenkinsci.plugins.workflow.libs.FolderLibraries
+import org.jenkinsci.plugins.workflow.libs.LibraryConfiguration
+import org.jenkinsci.plugins.workflow.libs.SCMSourceRetriever
+import org.jenkinsci.plugins.workflow.job.WorkflowJob
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition
+import com.synopsys.arc.jenkins.plugins.ownership.OwnershipDescription
+import com.synopsys.arc.jenkins.plugins.ownership.jobs.JobOwnerHelper
 
 
-jenkins.model.Jenkins.instance.doQuietDown();
+// jenkins.model.Jenkins.instance.doQuietDown();
 
+println "--> setting System Message"
 // parameters
 def systemMessage = "Hello! This is my custom Jenkins message."
 
@@ -25,20 +34,13 @@ def instance = Jenkins.getInstance()
 // set Jenkins system message
 instance.setSystemMessage(systemMessage)
 
-//get Jenkins configuration
-def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
-
-String adminAddress = "jenkins_admin@example.com"
-// set admin address
-jenkinsLocationConfiguration.setAdminAddress(adminAddress)
-jenkinsLocationConfiguration.save()
 
 // save current Jenkins state to disk
 instance.save()
 
-// SMTP
 def env = System.getenv()
 
+println "--> setting SMTP and admin email address"
 // Variables
 def SystemAdminMailAddress = env['SMTP_SYSAD_EMAIL']
 def SMTPUser = env['SMTP_USER']
@@ -50,9 +52,8 @@ def SMTPHost = env['SMTP_HOST']
 def mailServer = instance.getDescriptor("hudson.tasks.Mailer")
 def extmailServer = instance.getDescriptor("hudson.plugins.emailext.ExtendedEmailPublisher")
 
-
 //Jenkins Location
-println "--> Configuring JenkinsLocation"
+def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
 jenkinsLocationConfiguration.setAdminAddress(SystemAdminMailAddress)
 jenkinsLocationConfiguration.save()
 
@@ -74,11 +75,16 @@ extmailServer.defaultBody="\$PROJECT_NAME - Build # \$BUILD_NUMBER - \$BUILD_STA
 // Save the state
     instance.save()
 
+println "--> setting Github"
+
+GitHubServerConfig server = new GitHubServerConfig('')
+server.setName('GitHubAPI')
+server.setApiUrl('https://api.github.com')
+GitHubPlugin.configuration().getConfigs().add(server)
 
 // Git 
 
 def desc = instance.getDescriptor("hudson.plugins.git.GitSCM")
-
 desc.setGlobalConfigName(env['GIT_NAME'])
 desc.setGlobalConfigEmail(env['GIT_EMAIL'])
 
@@ -86,9 +92,22 @@ desc.save()
 
 // Folder
 
-def folder1 = Jenkins.instance.createProject(Folder.class, "folder1")
-def folder2 = Jenkins.instance.createProject(Folder.class, "folder1/folder2")
-def folder3 = Jenkins.instance.createProject(Folder.class, "folder3")
-// FolderOwnershipHelper.setOwnership(demoFolder, new OwnershipDescription(true, "user"))
+def folder1 = instance.createProject(Folder.class, "folder1")
+def folder2 = folder1.createProject(Folder.class, "folder2")
+def folder3 = instance.createProject(Folder.class, "folder3")
+def pipelineLibrarySource = new GitSCMSource("pipeline-library", "https://github.com/alex-kay/jenkins-shared-lib.git", null, null, null, false)
+LibraryConfiguration lc = new LibraryConfiguration("pipeline-library", new SCMSourceRetriever(pipelineLibrarySource))
+lc.setImplicit(true)
+lc.setDefaultVersion("master")
+folder1.addProperty(new FolderLibraries([lc]))
+folder1.setDescription("This folder has a shared library from https://github.com/alex-kay/jenkins-shared-lib")
+FolderOwnershipHelper.setOwnership(folder, new OwnershipDescription(true, "okurylo"))
+
+WorkflowJob sampleproject = folder1.createProject(WorkflowJob.class, "Ownership_Plugin_System_Master")
+sampleproject.setDefinition(new CpsFlowDefinition("buildPlugin(platforms: ['master'], repo: 'https://github.com/jenkinsci/ownership-plugin.git')", true))
+JobOwnerHelper.setOwnership(sampleproject, new OwnershipDescription(true, "okurylo"))
+
+instance.save()
 
 CASC_JENKINS_CONFIG="/jenkins/casc_configs"
+
