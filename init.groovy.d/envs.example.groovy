40272ac6d570d46c125881b0a4db3a16d8a74571
alex-kay <ichijoint@gmail.com>
2021-10-28 09:16:57 +0300

    update, forgot to commit

---
+++ init.groovy.d/envs.example.groovy
@@ -0,0 +1,9 @@
+env.DB_URL="hello"
+env.DB_URL2="hello2"
+env.JENKINS_SYSTEM_MESSAGE="Hello there! This is my system message."
+env.JENKINS_SYSTEM_ADMIN_EMAIL="admin@jenkins.com"
+env.JENKINS_SMTP_USER="admin"
+env.JENKINS_SMTP_PASSWORD="test1234"
+env.JENKINS_SMTP_PORT="25"
+env.JENKINS_SMTP_HOST="smtp.jenkins.com"
+
